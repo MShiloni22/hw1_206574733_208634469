b3fa2eca85d1ad80925959ffbd55191b9cc4c6ee
Matan Shiloni <mshiloni22@gmail.com>
2021-06-07 07:47:19 +0300

    started class Player

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,13 @@
+import java.util.*;
+
 public class Player {
+    private final String name;
+    Deck gameDeck;
+    Deck winDeck;
+
+    public Player(String name){
+        this.name = name;
+        this.gameDeck = new Deck(false);
+        this.winDeck = new Deck(false);
+    }
 }
